519194e88c3b7af7936f53e5fd86fa064ebd59d6
V|KAS <vks93@live.com>
2023-10-27 07:40:01 +0000

    Working on array code practice

---
+++ step-1/04-math.js
@@ -0,0 +1,10 @@
+//console.log(Math.round(Math.PI))
+
+// getting value showuld be between 3 to 28
+var min = 3
+var max = 28
+
+// this one is the random number in between 3 and 28
+var calculateRandomNumber = Math.round(Math.random() * (max - min))
+
+console.log(calculateRandomNumber)
